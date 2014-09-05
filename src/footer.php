bf80d18077eef374860eeaebde6a6779e7d4cd86
Riccardo Margiotta <riccardo.margiotta@gmail.com>
2014-09-05 15:29:08 +0100

    Initial commit of source assets.

---
+++ src/footer.php
@@ -0,0 +1,7 @@
+
+</div> <!-- /wrapper -->
+
+<footer></footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+
+
+  <div class="content-panel">
+    <div class="content-text">
+      <h2><a href="http://www.dantesrestaurant.co.uk/">Dantes</a></h2>
+      <p>Dantes is an Italian restaurant located in Edinburgh. The site features a fully responsive and fluid layout, and runs on WordPress. I was also responsible for the photography.</p>
+      <ul class="link-list">
+        <li class="link-url"><a href="http://www.dantesrestaurant.co.uk/">www.dantesrestaurant.co.uk</a></li>
+      </ul>
+    </div>
+    <img src="<?php bloginfo('template_directory'); ?>/images/assets/dantes1.jpg" alt="" class="content-image-main" />
+    <img src="<?php bloginfo('template_directory'); ?>/images/assets/dantes2.jpg" alt="" class="content-image-sub" />
+  </div>
+
+  <div class="content-panel">
+    <div class="content-text">
+      <h2><a href="http://caffeecucinaedinburgh.co.uk/">Caffe e Cucina</a></h2>
+      <p>Caffe e Cucina is an Italian coffee shop and restaurant located in Edinburgh. The site features a fully responsive and fluid layout, and runs on WordPress. Thanks to the friendly interface of the CMS, the non-technical restaurant owner is able to easily update the site with special menus and news items. I was also responsible for the photography.</p>
+      <ul class="link-list">
+        <li class="link-url"><a href="http://caffeecucinaedinburgh.co.uk/">www.caffeecucinaedinburgh.co.uk</a></li>
+      </ul>
+    </div>
+    <img src="<?php bloginfo('template_directory'); ?>/images/assets/cucina1.jpg" alt="" class="content-image-main" />
+    <img src="<?php bloginfo('template_directory'); ?>/images/assets/cucina2.jpg" alt="" class="content-image-sub" />
+  </div>
+
+  <div class="content-panel">
+    <div class="content-text">
+      <h2><a href="http://www.tescobank.com/">Tesco Bank</a></h2>
+      <p>While working at Tesco Bank, I was responsible for updating daily content changes and fixes to the site, and for launching new online promotional campaigns across a range of products every six weeks.</p>
+      <p>I was also part of the project team responsible for moving the Tesco Bank site to a new Responsive design, optimised for mobile, tablet and desktop devices at any screen resolution. This required me to optimise and migrate hundreds of pages and images to new templates, and to complete extensive site testing across multiple web browsers, browser versions, operating systems, mobile devices, and screen resolutions.</p>
+      <ul class="link-list">
+        <li class="link-url"><a href="http://www.tescobank.com/">www.tescobank.com</a></li>
+      </ul>
+    </div>
+    <img src="<?php bloginfo('template_directory'); ?>/images/assets/tescobank1.jpg" alt="" class="content-image-main" />
+    <img src="<?php bloginfo('template_directory'); ?>/images/assets/tescobank2.jpg" alt="" class="content-image-sub" />
+  </div>
+
+  <div class="content-panel">
+    <div class="content-text">
+      <h2>Stay tuned...</h2>
+      <p>My new site is a work in progress. Look out for case studies and more of my work soon.</p>
+    </div>
+  </div>
+
+<?php get_footer(); ?>
