eacb1337ac40b32f9b29d6815f9585e44cb5fb4c
Riccardo Margiotta <riccardo.margiotta@gmail.com>
2023-02-05 21:49:48 +0000

    Add code syntax highlighting.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
     dir: {
