c2a39f338e1a567d5c90e97c2f24183cc3e0df35
Riccardo Margiotta <riccardo.margiotta@gmail.com>
2023-02-07 21:18:29 +0000

    Build with Sass for deployments.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
