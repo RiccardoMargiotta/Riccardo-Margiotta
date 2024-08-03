436e9bfe9c41dafd72265e5505a8c5ef534a2f3c
Riccardo Margiotta <riccardo.margiotta@gmail.com>
2024-08-03 17:20:39 +0100

    Fix CSS live reloading.

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/images");
-  eleventyConfig.addWatchTarget("./src/assets/stylesheets/");
+  eleventyConfig.addWatchTarget("./src/assets/stylesheets/*");
   eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
